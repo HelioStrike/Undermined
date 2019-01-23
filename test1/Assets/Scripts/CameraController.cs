a2d0e83c0acd50050c9c15797883f5a33e1aac6d
HelioStrike <rageofepicfury@gmail.com>
2019-01-23 23:44:09 +0530

    committed

---
+++ test1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	GameObject player;
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+		offset = this.transform.position - player.transform.position;
+	}
+	
+	void LateUpdate () {
+		this.transform.position = offset + player.transform.position;
+	}
+}
--- test1/Assets/Scripts/Controller.cs
+++ test1/Assets/Scripts/Controller.cs
@@ -11,10 +11,15 @@ public class Controller : MonoBehaviour {
 	private int jumpsLeft = 2;
 	private bool canJump = true;
 	public float jumpGap;
+	public GameObject model;
+	private Animator anim;
+	private float horizontal, vertical;
+	private bool currDir = true, newDir; //
 
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody>();
+		anim = model.GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
@@ -23,8 +28,21 @@ public class Controller : MonoBehaviour {
 	}
 
 	void FixedUpdate() {
-		rb.velocity = new Vector3(Input.GetAxis("Horizontal")*speed,rb.velocity.y,0);
-		if(Input.GetAxis("Vertical") > 0 && canJump == true && jumpsLeft > 0) {
+		horizontal = Input.GetAxis("Horizontal");
+		vertical = Input.GetAxis("Vertical");
+
+		newDir = (horizontal > 0);
+		if(horizontal != 0 && newDir != currDir) {
+        	transform.eulerAngles = new Vector3(0, transform.eulerAngles.y + 180, 0);
+			currDir = newDir;
+		}
+
+		rb.velocity = new Vector3(horizontal*speed,rb.velocity.y,0);
+		if(horizontal != 0 && !anim.GetCurrentAnimatorStateInfo(0).IsName("Walk")) {
+			anim.Play("Walk",0,0.1f);
+		}
+
+		if(vertical > 0 && canJump == true && jumpsLeft > 0) {
 			jump();
 			jumpsLeft -= 1;
 			Invoke("resetCanJump", jumpGap);
