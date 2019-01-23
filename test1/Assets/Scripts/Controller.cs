e41c28a9638e32df24ce9b58c53a44e8bc7e00e5
HelioStrike <rageofepicfury@gmail.com>
2019-01-23 18:35:39 +0530

    committed

---
+++ test1/Assets/Scripts/Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public float jumpForce;
+	private long lastJump = 0;
+	private int jumpsLeft = 2;
+	private bool canJump = true;
+	public float jumpGap;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void FixedUpdate() {
+		rb.velocity = new Vector3(Input.GetAxis("Horizontal")*speed,rb.velocity.y,0);
+		if(Input.GetAxis("Vertical") > 0 && canJump == true && jumpsLeft > 0) {
+			jump();
+			jumpsLeft -= 1;
+			Invoke("resetCanJump", jumpGap);
+		}
+	}
+
+	void jump() {
+		rb.AddForce(new Vector3(0,jumpForce, 0));
+		canJump = false;
+	}
+
+	void resetCanJump() {
+		canJump = true;
+	}
+
+	void OnCollisionEnter(Collision collisionInfo)
+	{
+		jumpsLeft = 2;	
+	}
+}
